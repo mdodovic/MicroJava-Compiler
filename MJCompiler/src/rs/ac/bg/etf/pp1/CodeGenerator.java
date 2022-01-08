9b7d25319697d7b0096f55e86aa80affcad731d7
mdodovic <matija.dodovic@gmail.com>
2022-01-08 17:54:58 +0100

    add code generting for enter into function

--- MJCompiler/src/rs/ac/bg/etf/pp1/CodeGenerator.java
+++ MJCompiler/src/rs/ac/bg/etf/pp1/CodeGenerator.java
@@ -1,7 +1,11 @@
 package rs.ac.bg.etf.pp1;
 
+import rs.ac.bg.etf.pp1.CounterVisitor.FormParamCounter;
+import rs.ac.bg.etf.pp1.CounterVisitor.VarCounter;
 import rs.ac.bg.etf.pp1.ast.Const;
+import rs.ac.bg.etf.pp1.ast.MethodTypeName;
 import rs.ac.bg.etf.pp1.ast.PrintStmt;
+import rs.ac.bg.etf.pp1.ast.SyntaxNode;
 import rs.ac.bg.etf.pp1.ast.VisitorAdaptor;
 import rs.etf.pp1.mj.runtime.Code;
 import rs.etf.pp1.symboltable.Tab;
@@ -35,5 +39,28 @@ public class CodeGenerator extends VisitorAdaptor{
 		Code.load(con);
 	}
 	
+	
+	public void visit(MethodTypeName methodTypeName){
+		
+		if("main".equalsIgnoreCase(methodTypeName.getMethName())){
+			mainPc = Code.pc;
+		}
+
+		methodTypeName.obj.setAdr(Code.pc);
+		// Collect arguments and local variables
+		SyntaxNode methodNode = methodTypeName.getParent();
+	
+		VarCounter varCnt = new VarCounter();
+		methodNode.traverseTopDown(varCnt);
+		
+		FormParamCounter fpCnt = new FormParamCounter();
+		methodNode.traverseTopDown(fpCnt);
+		
+		// Generate the entry
+		Code.put(Code.enter);
+		Code.put(fpCnt.getCount());
+		Code.put(fpCnt.getCount() + varCnt.getCount());
+	
+	}
 	
 }

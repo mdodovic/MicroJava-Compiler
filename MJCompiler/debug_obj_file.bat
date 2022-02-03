623aeb69d0aed3883e6b19c3332216da5b60ac20
mdodovic <matija.dodovic@gmail.com>
2022-02-03 10:17:14 +0100

    disassemble and debug *.obj; pass *.obj name [script]

--- MJCompiler/debug_obj_file.bat
+++ MJCompiler/debug_obj_file.bat
@@ -1,3 +1,7 @@
-java -cp ./test;./lib/mj-runtime-1.1.jar rs.etf.pp1.mj.runtime.Run -debug test/program.obj
+cls
+
+java -cp ./test;./lib/mj-runtime-1.1.jar rs.etf.pp1.mj.runtime.disasm test/%1.obj
+
+java -cp ./test;./lib/mj-runtime-1.1.jar rs.etf.pp1.mj.runtime.Run -debug test/%1.obj
 
 pause

3ac75a03ee6c9f691407fa997388ba267fcf6803
mdodovic <matija.dodovic@gmail.com>
2022-02-04 18:04:52 +0100

    stop semantic analysis on parser error happened

--- MJCompiler/test/rs/ac/bg/etf/pp1/Compiler.java
+++ MJCompiler/test/rs/ac/bg/etf/pp1/Compiler.java
@@ -30,7 +30,7 @@ public class Compiler {
 		Reader br = null;
 		try {
 			
-			String mjFileName = "program";
+			String mjFileName = args[0];
 			
 			File sourceCode = new File("test/" + mjFileName + ".mj");
 			log.info("Compiling source file: " + sourceCode.getAbsolutePath());
@@ -39,9 +39,8 @@ public class Compiler {
 			Yylex lexer = new Yylex(br);
 			
 			MJParser p = new MJParser(lexer);
-	        Symbol s = p.parse();  //pocetak parsiranja
+	        Symbol s = p.parse();
 	        
-	        // Symbol table initialization
 	        Tab.init();
 	        
 	        Program prog = (Program)(s.value); 
@@ -88,108 +87,5 @@ public class Compiler {
 		
 	}
 	
-	private static void semanticAnalysis() throws Exception {
-		Logger log = Logger.getLogger(MJParserTest.class);
-		
-		Reader br = null;
-		try {
-			File sourceCode = new File("test/program.mj");
-			log.info("Compiling source file: " + sourceCode.getAbsolutePath());
-			
-			br = new BufferedReader(new FileReader(sourceCode));
-			Yylex lexer = new Yylex(br);
-			
-			MJParser p = new MJParser(lexer);
-	        Symbol s = p.parse();  //pocetak parsiranja
-	        Tab.init();
-	        Program prog = (Program)(s.value); 
-			// ispis sintaksnog stabla
-			log.info(prog.toString(""));
-			log.info("===================================");
-
-			// ispis prepoznatih programskih konstrukcija
-			// RuleVisitor v = new RuleVisitor();
-			
-			SemanticPass v = new SemanticPass();
-			prog.traverseBottomUp(v); 
-	      
-			log.info(" Print count calls = " + v.printCallCount);
-
-			log.info(" Deklarisanih promenljivih ima = " + v.varDeclCount);
-			
-			log.info("===================================");
-	        Tab.dump();
-	        
-	        if(!p.errorDetected && v.passed()) {
-				log.info("Parsiranje uspesno zavrseno!");
-			}else{
-				log.error("Parsiranje NIJE uspesno zavrseno!");
-			}
-	        
-		} 
-		finally {
-			if (br != null) try { br.close(); } catch (IOException e1) { log.error(e1.getMessage(), e1); }
-		}
-		
-	}
-
-	private static void codeGenerator() throws Exception {
-		Logger log = Logger.getLogger(MJParserTest.class);
-		
-		String mjFileName = "program";
-		
-		Reader br = null;
-		try {
-			File sourceCode = new File("test/" + mjFileName + ".mj");
-			log.info("Compiling source file: " + sourceCode.getAbsolutePath());
-			
-			br = new BufferedReader(new FileReader(sourceCode));
-			Yylex lexer = new Yylex(br);
-			
-			MJParser p = new MJParser(lexer);
-	        Symbol s = p.parse();  //pocetak parsiranja
-	        Tab.init();
-	        Program prog = (Program)(s.value); 
-			// ispis sintaksnog stabla
-			log.info(prog.toString(""));
-			log.info("===================================");
-
-			// ispis prepoznatih programskih konstrukcija
-			// RuleVisitor v = new RuleVisitor();
-			
-			SemanticPass v = new SemanticPass();
-			prog.traverseBottomUp(v); 
-	      
-			log.info(" Print count calls = " + v.printCallCount);
-
-			log.info(" Deklarisanih promenljivih ima = " + v.varDeclCount);
-			
-			log.info("===================================");
-	        Tab.dump();
-	        
-	        if(!p.errorDetected && v.passed()) {
-	        	
-				File objFile = new File("test/" + mjFileName + ".obj");
-				if(objFile.exists()) objFile.delete();
-				
-				CodeGeneratorTutorial codeGenerator = new CodeGeneratorTutorial();
-				prog.traverseBottomUp(codeGenerator);
-				
-				Code.dataSize = v.nVars;
-				Code.mainPc = codeGenerator.getMainPc();
-				Code.write(new FileOutputStream(objFile));
-	        	
-	        	log.info("Parsiranje uspesno zavrseno!");
-			}else{
-				log.error("Parsiranje NIJE uspesno zavrseno!");
-			}
-	        
-		} 
-		finally {
-			if (br != null) try { br.close(); } catch (IOException e1) { log.error(e1.getMessage(), e1); }
-		}
-		
-	}
-
 	
 }
